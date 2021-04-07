3273a4ed1e42bef6938c549a71a4610ddc2f4654
JinJin_Hrb <fanwang1987@gmail.com>
2021-04-07 15:49:05 +0800

    upgrade .umirc.js

--- .umirc.js
+++ .umirc.js
@@ -1,28 +1,43 @@
-
 // ref: https://umijs.org/config/
 export default {
-  plugins: [
-    // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: {
-        loading: true,
-      },
-      dynamicImport: true,
-      title: 'umi',
-      dll: true,
-      pwa: false,
-      routes: {
-        exclude: [],
-      },
-      hardSource: false,
-    }],
-  ],
-  proxy: {
-    "/api": {
-      "target": "http://localhost:8001",
-      "changeOrigin": true,
-      "pathRewrite": { "^/api" : "" }
-    }
+  nodeModulesTransform: {
+    type: 'none',
   },
-}
+  //   plugins: [
+  //     // ref: https://umijs.org/plugin/umi-plugin-react.html
+  //     ['umi-plugin-react', {
+  //       antd: true,
+  //       dva: {
+  //         loading: true,
+  //       },
+  //       dynamicImport: true,
+  //       title: 'umi',
+  //       dll: true,
+  //       pwa: false,
+  //       routes: {
+  //         exclude: [],
+  //       },
+  //       hardSource: false,
+  //     }],
+  //   ],
+  antd: {},
+  // dva: {
+  //     loading: true,
+  // },
+  dynamicImport: {},
+  title: 'umi',
+  // dll: true,
+  // pwa: false,
+  // routes: {
+  //     exclude: [],
+  // },
+  // hardSource: false,
+  proxy: {
+    '/api': {
+      target: 'http://localhost:8002',
+      changeOrigin: true,
+      pathRewrite: { '^/api': '' },
+    },
+  },
+  fastRefresh: {},
+};
